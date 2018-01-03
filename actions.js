d716737a04d959f9ef7a41593c2d303f8d45af85
Przemek <puza147@gmail.com>
2018-01-03 11:24:59 +0100

    actions.js

--- actions.js
+++ actions.js
@@ -1,31 +1,5 @@
 import uuid from uuid;
 
-{
-    type: ADD_COMMENT,
-    text: 'Comment added.'
-}
-
-{
-    type: EDIT_COMMENT,
-    id: 1,
-    text: 'Edited comment'
-}
-
-{
-    type: REMOVE_COMMENT,
-    id: 1
-}
-
-{
-    type: VOTE_UP,
-    id: 1
-}
-
-{
-    type: VOTE_DOWN,
-    id: 1
-}
-
 function addComment(text) {
     return {
         type: ADD_COMMENT,
@@ -68,3 +42,9 @@ const boundEditComment = (text, id) => dispatch(editComment(text, id));
 const boundRemoveComment = id => dispatch(editComment(id));
 const boundVoteUp = id => dispatch(voteUp(id));
 const boundVoteDown = id => dispatch(voteDown(id));
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const VOTE_UP = 'VOTE_UP';
+export const VOTE_DOWN = 'VOTE_DOWN';
