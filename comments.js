d835c4f973bdf43306f2c2e0b70b0831580e8c39
Przemek <puza147@gmail.com>
2018-01-08 12:38:13 +0100

    19_4

---
+++ comments.js
@@ -0,0 +1,47 @@
+import {ADD_COMMENT, REMOVE_COMMENT, EDIT_COMMENT, VOTE_UP, VOTE_DOWN} from './actions'
+
+function comments(state = [], action) {
+    switch(action.type) {
+        case ADD_COMMENT:
+            return [{
+                id: action.id,
+                text: action.text
+                votes: 0
+            }
+            , ...state.comments];
+    
+		case REMOVE_COMMENT:
+			return state.comments.filter(comment => comment.id !== action.id);
+		case EDIT_COMMENT:
+			return state.comments.map(function(comment) {
+                    if(action.id === comment.id) {
+                        comment.text = action.text;
+                        return comment;
+                    }
+                    return comment;
+                }
+			})
+        case VOTE_UP:
+            return state.comments.map(function(comment) {
+                    if(action.id === comment.id) {
+                        comment.votes += 1;
+                        return comment;
+                    }
+                    return comment;
+                }
+            })
+        case VOTE_DOWN:
+            return state.comments.map(function(comment) {
+                    if(action.id === comment.id) {
+                        comment.votes -= 1;
+                        return comment;
+                    }
+                    return comment;
+                }
+            })
+        default:
+            return state;
+    }
+}
+
+export default comments;
--- reducer.js
+++ reducer.js
@@ -1,61 +1,8 @@
-import {ADD_COMMENT} from './actions'
-import {REMOVE_COMMENT} from './actions'
-import {EDIT_COMMENT} from './actions'
-import {VOTE_UP} from './actions'
-import {VOTE_DOWN} from './actions'
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
 
-const initialState = {
-    comments: [],
-    users: []
-};
-
-function reducer(state = initialState, action) {
-    switch(action.type) {
-        case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    text: action.text,
-                    votes: 0
-                }
-                , ...state]
-            });
-		case REMOVE_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.filter(comment => comment.id !== action.id)
-			});
-		case EDIT_COMMENT:
-			return Object.assign({}, state, {
-                comments: state.comments.map(function(comment) {
-                    if(action.id === comment.id) {
-                        comment.text = action.text;
-                        return comment;
-                    }
-                    return comment;
-                })
-			})
-        case VOTE_UP:
-            return Object.assign({}, state, {
-                comments: state.comments.map(function(comment) {
-                    if(action.id === comment.id) {
-                        comment.votes += 1;
-                        return comment;
-                    }
-                    return comment;
-                })
-            })
-        case VOTE_DOWN:
-            return Object.assign({}, state, {
-                comments: state.comments.map(function(comment) {
-                    if(action.id === comment.id) {
-                        comment.votes -= 1;
-                        return comment;
-                    }
-                    return comment;
-                })
-            })
-        default:
-            return state;
-    }
-}
+const app = combineReducers({
+    comments,
+    users
+});
